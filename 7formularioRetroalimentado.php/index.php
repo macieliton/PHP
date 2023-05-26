1c64c64e51f6719abc8e85c0eabc8db16d912046
macieliton <maxcieliton@gmail.com>
2023-05-26 10:22:12 -0300

    up

--- 7formularioRetroalimentado.php/index.php
+++ 7formularioRetroalimentado.php/index.php
@@ -1,36 +1,32 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html>
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>exercicio php</title>
+    <title>formulário retroaliementado</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
     <script src='main.js'></script>
     <link rel="stylesheet" href="style.css">
-
 </head>
 <body>
     <?php 
-    // pegar os dados do formulário retro alimentado
-    $val1 = $_GET['v1'] ?? 0;
-    $val2 = $_GET['v2'] ?? 0;
+    $num1 = $_GET['num1'] ?? 0;
+    $num2 = $_GET['num2'] ?? 0;
     ?>
-    <main>
-        <form action="<?=$_SERVER['PHP_SELF']  ?>" method="get">
-        <label for="V1">Valor 1</label>
-        <input type="number" name="v1" id="v1" value="<?=$val1?>">
-        <label for="v2">Valor 2</label>
-        <input type="number" name="v2" id="v2" value="<?=$val2?>">
+    <section>
+        <form action="<?=$_SERVER['PHP_SELF']?>" method="get">
+        <label for="num1">valor1: </label>
+        <input type="number" name="num1" id="idnum1" value="<?=$num1?>">
+        <label for="num2">valor 2: </label>
+        <input type="number" name="num2" id="idnum2" value="<?=$num2?>">
         <input type="submit" value="somar">
         </form>
-    </main>  
-
-    <section id= "resultado":>
-        <h2>Resultado da Soma:</h2>
-        <?php 
-        $soma = $val1 + $val2;
-        echo "o resultado da soma é $soma";
+    </section>
+    <section>
+        <?php
+        $soma = $num1 + $num2;
+        echo "a soma de $num1 e $num2 é $soma!"
         ?>
     </section>
 </body>
